1518cd6683374fac5734c905c9fda7447d9aa57c
MichaelWaterfall <michaeljames.waterfall@gmail.com>
2025-07-02 14:16:37 +0100

    update function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let total = "";
+const totalDisplay = document.getElementById("display");
+totalDisplay.value = total;
 
 const zeroButton = document.getElementById("zeroButton");
 const oneButton = document.getElementById("oneButton");
@@ -10,3 +12,13 @@ const sixButton = document.getElementById("sixButton")
 const sevenButton = document.getElementById("sevenButton");
 const eightButton = document.getElementById("eightButton");
 const nineButton = document.getElementById("nineButton");
+
+function update() {
+    
+}
+
+zeroButton.addEventListener("click", () => {
+    console.log("0" + total);
+    total += "0";
+
+});
