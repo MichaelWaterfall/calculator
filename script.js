96c484c0ff704ff3520addc98c5ab8d8c123ce46
MichaelWaterfall <michaeljames.waterfall@gmail.com>
2025-07-02 13:03:46 +0100

    number buttons added

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 let total = "";
 
+const zeroButton = document.getElementById("zeroButton");
+const oneButton = document.getElementById("oneButton");
+const twoButton = document.getElementById("twoButton");
+const threeButton = document.getElementById("threeButton");
+const fourButton = document.getElementById("fourButton");
+const fiveButton = document.getElementById("fiveButton");
+const sixButton = document.getElementById("sixButton")
 const sevenButton = document.getElementById("sevenButton");
+const eightButton = document.getElementById("eightButton");
+const nineButton = document.getElementById("nineButton");
